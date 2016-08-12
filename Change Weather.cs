275e600d9909270bd899e171fc5b82c27a88927d
AbelSoftware <lcpdpolice4939@gmail.com>
2016-08-11 20:08:15 -0400

    Add files via upload

---
+++ Change Weather.cs
@@ -0,0 +1,4 @@
+//Extra Sunny
+World.Weather = Weather.ExtraSunny;
+
+//Use the Weather List from https://github.com/AbelSoftware/NativeXperience to get all weather types and just replace ExtraSunny
