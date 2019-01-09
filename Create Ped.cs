d29bf3f5c0c4a36fff55e0438294dda8ab37e341
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-09 09:04:18 -0500

    Update Create Ped.cs

--- Create Ped.cs
+++ Create Ped.cs
@@ -1,4 +1,3 @@
 Ped mora = World.CreatePed("MODEL_GOES_HERE", World.GetNextPositionOnStreet(Game.Player.Character.Position.Around(5f));
 
-//Use model list from https://github.com/AbelSoftware/NativeXperience and replace "MODEL_GOES_HERE" with the model you want to spawn the ped with
 //**Note**: This is not to change your player model. I have posted a different code for that!
