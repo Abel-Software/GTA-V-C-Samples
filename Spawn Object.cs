dee9f271ef93f5e99482a34600dcf29c98097068
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-09 09:05:41 -0500

    Update Spawn Object.cs

--- Spawn Object.cs
+++ Spawn Object.cs
@@ -3,4 +3,3 @@ Prop obj = World.CreateProp("prop_log_01", (Game.Player.Character.Position.Aroun
 //Create a non-dynamic object with being place on the ground
 //false = dynamic object (Change to true if you want the object to be dynamic)
 //true = place on ground (change to false if you do not want the object to place on the ground)
-//Use object list from https://github.com/AbelSoftware/NativeXperience and replace "prop_log_01" with the object idea
