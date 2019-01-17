bafcbb57fd3ea3e858591665d6f7644622dfb924
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-17 00:35:30 -0500

    Delete Create Ped.cs

--- Create Ped.cs
+++
@@ -1,3 +0,0 @@
-Ped mora = World.CreatePed("MODEL_GOES_HERE", World.GetNextPositionOnStreet(Game.Player.Character.Position.Around(5f));
-
-//**Note**: This is not to change your player model. I have posted a different code for that!
