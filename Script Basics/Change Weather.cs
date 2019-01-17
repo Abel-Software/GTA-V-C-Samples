d6fd59ddb4715b35e19533527dc56f0679cd7b4b
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-17 00:28:09 -0500

    Create Change Weather.cs

---
+++ Script Basics/Change Weather.cs
@@ -0,0 +1,18 @@
+World.Weather = Weather.ExtraSunny;
+
+//Weather Types
+//Blizzard
+//Christmas
+//Clear
+//Clearing
+//Clouds
+//ExtraSunny
+//Foggy
+//Halloween
+//Neutral
+//Overcast
+//Raining
+//Smog
+//Snowing
+//Snowlight
+//ThunderStorm
