029bc6caa0ba7b0f25cf0bccfef512bfa465a95d
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-17 00:38:24 -0500

    Rename Teleport Player.cs to Script Basics/Teleport Player.cs

--- Teleport Player.cs
+++
@@ -1,3 +0,0 @@
-Game.Player.Character.Position = new GTA.Math.Vector3(568.861f, -3123.702f, 18.769f);
-
-//Use this mod to obtain coordinates https://www.gta5-mods.com/tools/coordinates-recorder-net
