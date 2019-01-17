87f4a72839fca115f2d3a1ae0aab3f84c82003dd
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-17 10:50:58 -0500

    Create Clear Wanted Level.cs

---
+++ Script Examples/Clear Wanted Level.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+namespace ModName
+{
+    public class Main : Script
+    {
+        string ModName = "Clear Wanted Level";
+        string Developer = "Abel Software";
+        string Version = "1.0";
+
+        public Main()
+        {
+            Tick += onTick;
+            KeyDown += onKeyDown;
+            Interval = 1;
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F3)
+            {
+                if (Game.Player.WantedLevel == 0)
+                {
+                    UI.ShowSubtitle("~r~You have no wanted levels!");
+                }
+                else
+                {
+                    Game.Player.WantedLevel = 0;
+                }
+            }
+        }
+    }
+}
+
+namespace MyFirstMod
+{
+    public class Class1
+    {
+        
+    }
+}
