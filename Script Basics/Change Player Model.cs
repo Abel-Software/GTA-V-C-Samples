04ff2daa3fe2dad2e8ba68259082c768f990ea0c
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-17 04:42:07 -0500

    Create Change Player Model.cs

---
+++ Script Basics/Change Player Model.cs
@@ -0,0 +1,6 @@
+Game.Player.ChangeModel(PedHash.Deer);
+or
+Game.Player.ChangeModel("S_M_Y_XMECH_01");
+
+//Model list can be found here > https://github.com/Abel-Software/GTA-V-C-Samples/blob/master/Script%20Basics/Models%20List.txt
+//I will post ped hashes later!
