cc396201d6d5dddfec3f114f995592f2f14b3f55
Tyler Abel <lcpdpolice4939@gmail.com>
2019-01-17 04:43:55 -0500

    Update Spawn Object.cs

--- Script Basics/Spawn Object.cs
+++ Script Basics/Spawn Object.cs
@@ -1,4 +1,4 @@
-Prop obj = World.CreateProp("prop_mp_arrow_barrier_01", Game.Player.Character.Position, false, true);
+Prop obj = World.CreateProp("prop_mp_barrier_01", Game.Player.Character.Position, Game.Player.Character.Rotation, false, true);
 
 //Create a non-dynamic object with being place on the ground
 //false = dynamic object (Change to true if you want the object to be dynamic)
